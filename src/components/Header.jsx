138e3ebd2de67ad969aaeecd60ffebe9226c9188
Vaiterius <gerard.gandionco@gmail.com>
2023-11-19 04:38:30 -0800

    Add sections and resume pdf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = ({ navItemStyle }) => {
         <header>
             <nav className="navbar fixed top-0 flex flex-row flex-grow justify-end">
                 <div className={navItemStyle}><a href="#intro">Home</a></div>
-                <div className={navItemStyle}><a href="#about-me">About Me</a></div>
+                <div className={navItemStyle}><a href="#background">Background</a></div>
                 <div className={navItemStyle}><a href="#projects">Projects</a></div>
                 <div className={navItemStyle}><a href="/blog">Blog</a></div>
                 <div className={navItemStyle}><a href="#contact">Contact</a></div>
