c6a267968fb1838d0ba42ec92199d1512d9a135e
Vaiterius <gerard.gandionco@gmail.com>
2023-11-21 04:23:18 -0800

    Adjust various responsive spacings

--- src/components/HorizontalRule.jsx
+++ src/components/HorizontalRule.jsx
@@ -1,5 +1,5 @@
 const HorizontalRule = () => {
-	return <hr className="my-4 w-12 border-t-2 border-primary" />;
+	return <hr className="my-4 hidden w-12 border-t-2 border-primary md:flex" />;
 };
 
 export default HorizontalRule;
