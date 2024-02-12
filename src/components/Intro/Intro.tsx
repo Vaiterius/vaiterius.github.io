6b42f802c202eabaef49c1d93a2d0447c6a0895a
Vaiterius <gerard.gandionco@gmail.com>
2024-02-11 22:12:05 -0800

    Tweak intro section

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,16 +3,18 @@ import HorizontalRule from "../HorizontalRule";
 
 const Intro = () => {
 	return (
-		<div className="w-full lg:w-6/12">
+		<div className="w-full lg:w-8/12">
 			<div>
+				{/* <Fade> */}
 				<p className="text-primary">Hey, my name is...</p>
 				<h1 className="mb-2 text-6xl font-bold md:mb-0">Gerard Gandionco</h1>
+				{/* </Fade> */}
 			</div>
 			<HorizontalRule />
 			<p>
 				I'm an aspiring <span className="font-bold text-primary">full-stack developer</span>
-				, dedicated to blending innovative solutions with robust technical skills across
-				both <span className="font-bold text-secondary">front-end</span> and{" "}
+				, committed to merging solid technical skills across both{" "}
+				<span className="font-bold text-secondary">front-end</span> and{" "}
 				<span className="font-bold text-secondary">back-end</span> technologies. Proficient
 				in <span className="font-bold text-accent">Python</span>, I'm passionate about
 				creating interesting projects in my free time using an array of different
