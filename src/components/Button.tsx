98312e4b562f74fb8d99d89ce3db9a39f79b569e
Vaiterius <gerard.gandionco@gmail.com>
2024-02-13 23:23:28 -0800

    Attempt google analytics integration

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,16 @@
+import ReactGA from "react-ga4";
+
 const Button = (props: { text: string; className: string; url: string; newTab: boolean }) => {
+	const handleClick = () =>
+		ReactGA.event({
+			action: `Clicked ${props.text} on link ${props.url}`,
+			category: "ButtonClick",
+		});
 	return (
 		<a target={props.newTab ? "_blank" : undefined} href={props.url}>
-			<button className={props.className}>{props.text}</button>
+			<button className={props.className} onClick={handleClick}>
+				{props.text}
+			</button>
 		</a>
 	);
 };
